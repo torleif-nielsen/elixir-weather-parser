13f01af8ff201be2ddbd491fbfbbd899e2782617
Torleif Nielsen <torleif@hawaii.edu>
2016-07-21 11:15:23 -1000

    Add basic test

--- lib/weather/cli.ex
+++ lib/weather/cli.ex
@@ -1,16 +1,27 @@
 defmodule Weather.CLI do
   require Record
   Record.defrecord :xmlElement, Record.extract(:xmlElement, from_lib: "xmerl/include/xmerl.hrl")
-  Record.defrecord :xmlText,    Record.extract(:xmlText,    from_lib: "xmerl/include/xmerl.hrl")
+  Record.defrecord :xmlText, Record.extract(:xmlText, from_lib: "xmerl/include/xmerl.hrl")
 
   def parse(argv) do
     argv
+    |> parse_args()
     |> process
+  end
+
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [help: :boolean],
+                        aliases: [h: :help])
+    case parse do
+      {[help: true], _, _} -> :help
+      {_, [loc_code], _} -> loc_code
+      _ -> :help
+    end
   end
 
   def process(:help) do
     IO.puts """
-    usage: weather <location_code>
+    usage: weather <4-digit location_code>
     """
     System.halt(0)
   end
